d6fc3f0d34a921b5e956921f4a6142cb6a483999
Jakkapat Hongchakorn <67319010028@udontech.ac.th>
2026-01-12 17:16:46 +0700

    Initial commit

---
+++ player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+@export var speed := 120
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var last_direction := Vector2.DOWN
+func _physics_process(delta):
+	var direction := Vector2.ZERO
+
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		last_direction = direction
+		play_walk_animation(direction)
+	else:
+		play_idle_animation()
+
+	velocity = direction * speed
+	move_and_slide()
+func play_walk_animation(dir: Vector2):
+	if abs(dir.x) > abs(dir.y):
+		if dir.x > 0:
+			sprite.play("walk_right")
+		else:
+			sprite.play("walk_left")
+	else:
+		if dir.y > 0:
+			sprite.play("walk_down")
+		else:
+			sprite.play("walk_up")
+			
+func play_idle_animation():
+	if abs(last_direction.x) > abs(last_direction.y):
+		if last_direction.x > 0:
+			sprite.play("idle_right")
+		else:
+			sprite.play("idle_left")
+	else:
+		if last_direction.y > 0:
+			sprite.play("idle_down")
+		else:
+			sprite.play("idle_up")
